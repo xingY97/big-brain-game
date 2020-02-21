58bb0085e1910826e9b82c58b5af5c7700770364
Xing_Ye <YOUR_PERSONAL_EMAIL@example.com>
2020-02-20 16:56:14 -0800

    basic UI

---
+++ brainGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  brainGame
+//
+//  Created by XIN on 2/20/20.
+//  Copyright © 2020 XIN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
